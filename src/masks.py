16fbf0b25c4a0f1f2529bcd272b2fc1e02fa533f
oleg_fayzullin <olegin240678@mail.ru>
2024-06-10 13:31:18 +0300

    gitignore fixes

---
+++ src/masks.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+
+def get_mask_card_number(card_number: str) -> str | None:
+    """Возвращает замаскированный номер карты в виде строки"""
+    if card_number.isdigit() and len(card_number) == 16:
+        return f"{card_number[:5]} {card_number[5:7]}{"*" * 2} {"*" * 4} {card_number[12:]}"
+    else:
+        return None
+
+
+def get_mask_account(acc_number: str) -> str | None:
+    """Возвращает замаскированный номер счета в виде строки"""
+    if acc_number.isdigit() and len(acc_number) == 20:
+        return f"{'*' * 2}{acc_number[-4::]}"
+    else:
+        return None
