a33ba36c090782e86a2e0aeccdb86cefd74a9562
oleg_fayzullin <olegin240678@mail.ru>
2024-06-17 13:55:07 +0300

    modified src/masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,17 +1,13 @@
-from typing import Any
+def get_mask_card_number(card_number: str) -> str:
+    """Принимает на вход номер карты и возвращает ее маску."""
+    return card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
 
 
-def get_mask_card_number(card_number: str) -> str | None:
-    """Возвращает замаскированный номер карты в виде строки"""
-    if card_number.isdigit() and len(card_number) == 16:
-        return f"{card_number[:5]} {card_number[5:7]}{"*" * 2} {"*" * 4} {card_number[12:]}"
-    else:
-        return None
+def get_mask_account(account_number: str) -> str:
+    """Принимает на вход номер счета и возвращает его маску."""
+    return "**" + account_number[-4:]
 
 
-def get_mask_account(acc_number: str) -> str | None:
-    """Возвращает замаскированный номер счета в виде строки"""
-    if acc_number.isdigit() and len(acc_number) == 20:
-        return f"{'*' * 2}{acc_number[-4::]}"
-    else:
-        return None
+if __name__ == "__main__":
+    print(get_mask_card_number('7000792289606361'))
+    print(get_mask_account('73654108430135874305'))
