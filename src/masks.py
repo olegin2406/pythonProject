df9c61b70c299468be99a2a34102d572c6d2c644
oleg_fayzullin <olegin240678@mail.ru>
2024-06-21 10:16:10 +0300

    modified:    src/masks.py

--- src/masks.py
+++ src/masks.py
@@ -9,5 +9,5 @@ def get_mask_account(account_number: str) -> str:
 
 
 if __name__ == "__main__":
-    print(get_mask_card_number('7000792289606361'))
-    print(get_mask_account('73654108430135874305'))
+    print(get_mask_card_number("7000792289606361"))
+    print(get_mask_account("73654108430135874305"))
