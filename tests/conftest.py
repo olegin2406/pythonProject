45244e8ad0d8249b6eb1e7f1dff93a0b020dd610
oleg_fayzullin <olegin240678@mail.ru>
2024-06-27 12:52:29 +0300

    modified tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,24 @@
 import pytest
-from src.processing import filter_by_state, sort_by_date, original_state
+
 
 @pytest.fixture
-def test_original_state():
-    return 'EXECUTED'
+def executed_state():
+    return "EXECUTED"
+
 
 @pytest.fixture
-def test_original_state_1():
-    return original_state
+def operations():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+@pytest.fixture
+def executed_operations():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
