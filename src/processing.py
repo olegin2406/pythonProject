73fd506ae959ef97d06441f1da9237eb466ba932
oleg_fayzullin <olegin240678@mail.ru>
2024-06-27 12:51:21 +0300

    modified src/processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,5 @@
 from typing import Any
 
-original_state = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
 
 def filter_by_state(inform_state: list[dict[str, Any]], state_id: str = "EXECUTED") -> list[dict[str, Any]]:
     """Функция которая принимает на вход список словарей и значений для ключа
@@ -29,5 +22,12 @@ def sort_by_date(inform_state: list[dict[str, Any]], reverse_list: bool = True) 
 
 
 if __name__ == "__main__":
+    original_state = [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
     print(filter_by_state(original_state))
     print(sort_by_date(original_state))
