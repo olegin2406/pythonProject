782cfa68956cc1dde135e89c197aac9337c4a17c
oleg_fayzullin <olegin240678@mail.ru>
2024-07-03 16:38:37 +0300

    init

--- src/decorators.py
+++ src/decorators.py
@@ -1,9 +1,10 @@
 def log(filename=None):
     """Декоратор для логирования вызовов функции.
-       Параметры:
-       filename (str): Имя файла для записи логов. Если не указано, логи выводятся в консоль.
-       Возвращает:
-       function: Обёртка для декорируемой функции."""
+    Параметры:
+    filename (str): Имя файла для записи логов. Если не указано, логи выводятся в консоль.
+    Возвращает:
+    function: Обёртка для декорируемой функции."""
+
     def decorator(func):
         def wrapper(*args, **kwargs):
             try:
@@ -21,12 +22,15 @@ def log(filename=None):
                 else:
                     print(f"my_function error: {e}. Inputs: {args}, {kwargs}")
                 raise
+
         return wrapper
+
     return decorator
 
 
 @log()
 def my_function(x, y):
     return x / y
+
 
 # my_function(3, 0)
