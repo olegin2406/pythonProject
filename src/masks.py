759bce27f648a97e96be923be04832a1b97a7e3c
oleg_fayzullin <olegin240678@mail.ru>
2024-07-30 18:38:24 +0300

    init

--- src/masks.py
+++ src/masks.py
@@ -26,6 +26,6 @@ def get_mask_account(account_number: str) -> str:
         logger.error(f"Произошла ошибка: {ex}")
 
 
-if __name__ == "__main__":
-    print(get_mask_card_number("7000792289606361"))
-    print(get_mask_account("73654108430135874305"))
+# if __name__ == "__main__":
+#     print(get_mask_card_number("7000792289606361"))
+#     print(get_mask_account("73654108430135874305"))
